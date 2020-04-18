647f93818bdcfb7229504ab1b0ae9b64beb85e73
NantipatSoftEn <keymasterviriya1150@gmail.com>
2020-04-18 17:54:43 +0700

    FIX FUCKING HELL " " to ''

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Cats } from './cats/cats.model';
 import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
 @Module({
     imports: [
         SequelizeModule.forRoot({
@@ -17,6 +18,7 @@ import { CatsModule } from './cats/cats.module';
             synchronize: true,
         }),
         CatsModule,
+        UsersModule
     ],
 })
 export class AppModule {}
